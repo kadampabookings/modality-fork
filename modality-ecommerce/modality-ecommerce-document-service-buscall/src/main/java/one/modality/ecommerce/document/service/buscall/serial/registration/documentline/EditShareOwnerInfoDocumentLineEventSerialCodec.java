ab59c401fbb9bb17aff9cad244c173db90d20327
Bruno Salmon <dev.salmonb@gmail.com>
2026-05-13 16:44:32 +0100

    Updated EditShareOwnerInfoDocumentLineEvent for the public talk (setting quantity without providing mates)

--- modality-ecommerce/modality-ecommerce-document-service-buscall/src/main/java/one/modality/ecommerce/document/service/buscall/serial/registration/documentline/EditShareOwnerInfoDocumentLineEventSerialCodec.java
+++ modality-ecommerce/modality-ecommerce-document-service-buscall/src/main/java/one/modality/ecommerce/document/service/buscall/serial/registration/documentline/EditShareOwnerInfoDocumentLineEventSerialCodec.java
@@ -13,6 +13,7 @@ public final class EditShareOwnerInfoDocumentLineEventSerialCodec extends Abstra
     private static final String CODEC_ID = "EditShareOwnerInfoDocumentLineEvent";
 
     private static final String MATES_NAMES_KEY = "matesNames";
+    private static final String QUANTITY_KEY    = "qty";
 
     public EditShareOwnerInfoDocumentLineEventSerialCodec() {
         super(EditShareOwnerInfoDocumentLineEvent.class, CODEC_ID);
@@ -22,10 +23,19 @@ public final class EditShareOwnerInfoDocumentLineEventSerialCodec extends Abstra
     public void encode(EditShareOwnerInfoDocumentLineEvent o, AstObject serial) {
         super.encode(o, serial);
         encodeStringArray(serial, MATES_NAMES_KEY, o.getMatesNames());
+        if (o.getQuantity() > 0)
+            encodeInteger(serial, QUANTITY_KEY, o.getQuantity());
     }
 
     @Override
     public EditShareOwnerInfoDocumentLineEvent decode(ReadOnlyAstObject serial) {
+        int quantity = decodeInteger(serial, QUANTITY_KEY, 0);
+        if (quantity > 0)
+            return postDecode(new EditShareOwnerInfoDocumentLineEvent(
+                decodeDocumentPrimaryKey(serial),
+                decodeDocumentLinePrimaryKey(serial),
+                quantity
+            ), serial);
         return postDecode(new EditShareOwnerInfoDocumentLineEvent(
             decodeDocumentPrimaryKey(serial),
             decodeDocumentLinePrimaryKey(serial),
